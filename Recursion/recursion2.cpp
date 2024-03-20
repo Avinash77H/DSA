ecf799a676ddf428892fcdd063af9c1815ac7433
Avinash77H <avinashrakholiya2@gmail.com>
2024-03-20 12:14:34 +0530

    20/03/2024

---
+++ Recursion/recursion2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void reachHome(int src,int des){
+    if(des==src){
+        cout<<"pahoch gaya"<<endl;
+        return ;
+    }
+    cout<<"src->"<<src<<" des->"<<des<<endl;
+    // src++;
+    reachHome(++src,des);
+}
+int main(){
+    int des=10;
+    int src=1;
+    reachHome(src,des);
+    return 0;
+}
